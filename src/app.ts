60845affa8d182b0e52b3e5733f62e7b872be8fd
Rogermferr <rogermferr@gmail.com>
2023-07-28 15:29:43 -0300

    fix: add email field in contacts

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    // origin: "https://kenzie-kontacts.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://kenzie-kontacts.vercel.app",
   })
 );
 
