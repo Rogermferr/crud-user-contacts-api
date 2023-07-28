970c3f8f10e51ae80eba041c1b4445fefae680be
Rogermferr <rogermferr@gmail.com>
2023-07-28 16:21:33 -0300

    fix: email middleware from contacts

--- src/middlewares/ensureEmailExists.middleware.ts
+++ src/middlewares/ensureEmailExists.middleware.ts
@@ -3,6 +3,7 @@ import { Repository } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { AppError } from "../errors/errors";
 import User from "../entities/users.entity";
+import Contact from "../entities/contacts.entity";
 
 const ensureEmailExistsMiddleware = async (
   req: Request,
@@ -13,8 +14,16 @@ const ensureEmailExistsMiddleware = async (
   const userEmail: string = res.locals.userEmail;
 
   const userRepository: Repository<User> = AppDataSource.getRepository(User);
+  const contactRepository: Repository<Contact> =
+    AppDataSource.getRepository(Contact);
 
   const user: User | null = await userRepository.findOneBy({ email: email });
+
+  const contact: Contact | null = await contactRepository.findOneBy({
+    email: email,
+  });
+
+  if (contact && email) throw new AppError("Email already exists", 409);
 
   if (user && userEmail === email) {
     return next();
--- src/schemas/contacts.schemas.ts
+++ src/schemas/contacts.schemas.ts
@@ -1,5 +1,4 @@
 import { z } from "zod";
-import { userSchema } from "./users.schemas";
 
 const contactSchema = z.object({
   id: z.string(),
