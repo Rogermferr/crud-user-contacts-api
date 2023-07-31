b59ae3f1384a944ef28f15c832e66e98c79194b3
Rogermferr <rogermferr@gmail.com>
2023-07-31 20:18:15 -0300

    feat: configuration from deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,7 +11,8 @@ const settings = (): DataSourceOptions => {
 
   const dbUrl: string | undefined = process.env.DATABASE_URL;
 
-  if (!dbUrl) throw new Error("Missing env var: 'DATABASE_URL'");
+  if (!dbUrl) throw new Error("Missing env var: 'DATABASE_URL'")
+
 
   return {
     type: "postgres",
